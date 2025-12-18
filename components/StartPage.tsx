815dcc883a2994dabeb731c7267f0ae42441897a
Trimpsuz <me@trimpsuz.dev>
2025-12-18 16:20:21 +0200

    feat: prevent small screens

--- components/StartPage.tsx
+++ components/StartPage.tsx
@@ -1,8 +1,10 @@
+'use client';
 import { useRef, useState } from 'react';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { motion } from 'motion/react';
 import { AuroraBackground } from './ui/aurora-background';
+import useWindowDimensions from '@/lib/hooks/useWindowDimensions';
 
 export default function StartPage({
   username,
@@ -15,6 +17,8 @@ export default function StartPage({
   onSearch: () => void;
   setIsActive: (isActive: boolean) => void;
 }) {
+  const { width } = useWindowDimensions();
+
   const [hovered, setHovered] = useState(false);
   const hoveredRef = useRef(false);
   const swapTimeout = useRef<NodeJS.Timeout | null>(null);
@@ -28,7 +32,7 @@ export default function StartPage({
         transition={{ delay: 0.3, duration: 0.8, ease: 'easeInOut' }}
         className="relative flex flex-col gap-12 items-center justify-center px-4"
       >
-        <div className="text-3xl md:text-7xl tracking-wide [word-spacing:-1.5rem] font-bold text-foreground text-center font-mono">
+        <div className="text-7xl tracking-wide [word-spacing:-1.5rem] font-bold text-foreground text-center font-mono">
           your{' '}
           <span
             className="inline-block align-baseline relative mh-[1.2em] mw-[4ch]"
@@ -64,35 +68,39 @@ export default function StartPage({
           in code.
         </div>
 
-        <div className="flex flex-row items-center gap-2 ">
-          <Input
-            onMouseOver={() => {
-              setIsActive(true);
-            }}
-            onMouseLeave={() => {
-              setIsActive(false);
-            }}
-            className="text-2xl! h-12! text-foreground!"
-            placeholder="username"
-            name="username"
-            autoComplete="off"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <Button
-            onMouseOver={() => {
-              setIsActive(true);
-            }}
-            onMouseLeave={() => {
-              setIsActive(false);
-            }}
-            size="xl"
-            className="cursor-pointer"
-            onClick={onSearch}
-            disabled={!username || !/^[0-9A-Za-z_]{2,32}$/.test(username)}
-          >
-            show me
-          </Button>
-        </div>
+        {width > 1850 && /Chrome/.test(navigator.userAgent) ? (
+          <div className="flex flex-row items-center gap-2">
+            <Input
+              onMouseOver={() => {
+                setIsActive(true);
+              }}
+              onMouseLeave={() => {
+                setIsActive(false);
+              }}
+              className="text-2xl! h-12! text-foreground!"
+              placeholder="username"
+              name="username"
+              autoComplete="off"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Button
+              onMouseOver={() => {
+                setIsActive(true);
+              }}
+              onMouseLeave={() => {
+                setIsActive(false);
+              }}
+              size="xl"
+              className="cursor-pointer"
+              onClick={onSearch}
+              disabled={!username || !/^[0-9A-Za-z_]{2,32}$/.test(username)}
+            >
+              show me
+            </Button>
+          </div>
+        ) : (
+          <div className="text-2xl">sorry, your browser is not supported or your screen is too small</div>
+        )}
       </motion.div>
     </AuroraBackground>
   );
